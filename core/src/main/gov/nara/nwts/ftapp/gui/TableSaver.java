6ec009ca4ac77c2fbc6002a8b23f724b2febc769
Terry Brady <terrywbrady@gmail.com>
2014-02-28 17:51:19 -0500

    cleanup

--- core/src/main/gov/nara/nwts/ftapp/gui/TableSaver.java
+++ core/src/main/gov/nara/nwts/ftapp/gui/TableSaver.java
@@ -23,7 +23,6 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
